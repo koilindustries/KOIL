218f76ed283051b275a9c656eabd11d01738fb49
koilindustries <69868400+koilindustries@users.noreply.github.com>
2020-08-20 11:48:19 -0500

    please

---
+++ wp-content/plugins/static-html-output-plugin/vendor/sabberworm/php-css-parser/tests/quickdump.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+
+#### Input
+
+```css
+
+```
+
+#### Structure (`var_dump()`)
+
+```php
+object(Sabberworm\CSS\CSSList\Document)#5 (3) {
+  ["aComments":protected]=>
+  array(0) {
+  }
+  ["aContents":protected]=>
+  array(0) {
+  }
+  ["iLineNo":protected]=>
+  int(1)
+}
+
+```
+
+#### Output (`render()`)
+
+```css
+
+```
